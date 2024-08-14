d441bfc1a92e55048fcf7a7a6180ada496a40a6c
danhhuy <huydanh0408@gmail.com>
2024-08-14 08:32:15 +0700

    UI Header #1: Logo - Search

--- src/components/layouts/DefaultLayout/Header/Header.js
+++ src/components/layouts/DefaultLayout/Header/Header.js
@@ -1,12 +1,30 @@
 import classNames from 'classnames/bind';
 
+import { SearchIcon, ClearIcon, LoadingIcon } from '~/components/Icons';
+import images from '~/assets/images';
 import styles from './Header.module.scss';
+
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <div className={cx('wrapper')}>
-            <div className={cx('inner')}>Header Default Layout</div>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="TikTok logo" />
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" />
+                    <span className={cx('clear')}>
+                        <ClearIcon />
+                    </span>
+                    <span className={cx('loading')}>
+                        <LoadingIcon />
+                    </span>
+                    <span className={cx('search-btn')}>
+                        <SearchIcon />
+                    </span>
+                </div>
+                <div className={cx('action')}>Action</div>
+            </div>
         </div>
     );
 }
